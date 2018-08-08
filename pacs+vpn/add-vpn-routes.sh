23ac7802d7ceaaaa8ceff919242d01a1f081776f
Marcelo Aguero <marceloaguero@gmail.com>
2018-08-08 09:13:09 -0300

    mejorar el script de rutas hacia openvpn

--- pacs+vpn/add-vpn-routes.sh
+++ pacs+vpn/add-vpn-routes.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+# Get openvpn container's IP address
 IP_GW=$(docker exec -it pacsvpn_openvpn_1 ifconfig | grep 10.10.10 | cut -d ":" -f 2 | cut -d " " -f 1)
-echo $IP_GW
-echo "docker exec -it pacsvpn_arc_1 ip route add 192.168.255.0/24 via $IP_GW" 
-docker exec -it pacsvpn_arc_1 ip route add 192.168.255.0/24 via $IP_GW
-echo "docker exec -it pacsvpn_viewer_1 ip route add 192.168.255.0/24 via $IP_GW"
-docker exec -it pacsvpn_viewer_1 ip route add 192.168.255.0/24 via $IP_GW
+
+# Check if the route to openvpn container exists in dcm4chee-arc container
+ROUTE_EXISTS=$(docker exec -it pacsvpn_arc_1 ip route | grep "$IP_GW")
+# If the route does not exist, the add it to arc container
+if [ -z "$ROUTE_EXISTS" ]; then
+    docker exec -it pacsvpn_arc_1 ip route add 192.168.255.0/24 via $IP_GW
+fi
+
+# Check if the route to openvpn container exists in viewer container
+ROUTE_EXISTS=$(docker exec -it pacsvpn_viewer_1 ip route | grep "$IP_GW")
+# If the route does not exist, the add it to viewer container
+if [ -z "$ROUTE_EXISTS" ]; then
+    docker exec -it pacsvpn_viewer_1 ip route add 192.168.255.0/24 via $IP_GW
+fi
